577645437e43a0d4852cc402d3611153d805a867
RamiroMM21 <105456306+RamiroMM21@users.noreply.github.com>
2022-05-19 11:50:51 -0700

    excepciones
    programa 1 de excepciones

---
+++ excepciones-cSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enteranumber:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+                Console.ReadKey();
+            }
+            catch (FormatException e)
+            {
+                Console.Write(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
